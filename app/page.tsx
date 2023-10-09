a387d49625ddfa47da5fe92128cd692aab3825e3
Taiwo oyetade <oyetadetaiwo44@gmail.com>
2023-10-09 23:52:34 +0100

    add: container element to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button"
+import Container from "@/layouts/Container"
 
 export default function Home() {
   return (
-    <>
+    <Container>
       <Button>craftopia</Button>
-    </>
+    </Container>
   )
 }
