8981ce3d511d2eea2eb7de965be349e8632ea3b4
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-10-09 21:37:04 +0100

    fixed

---
+++ app/(group)/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {SignupFeat} from '@/features/auth'
+
+const signup = () => {
+  return (
+    <div>
+      <SignupFeat/>
+    </div>
+  )
+}
+
+export default signup
