eafe6c3f8bc1719d402791c006412ea91428fc07
SardiusJay <abidoyejay@gmail.com>
2023-10-11 00:01:08 +0100

    wip: add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
 import Container from "@/layouts/Container"
+import Header from "@/components/header"
 
 export default function Home() {
   return (
---
+++ components/header.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+const header = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link href="/"><a>Home</a>
+          </Link>
+        </li>
+        <li><Link href="/about"><a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default header;
