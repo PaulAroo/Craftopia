2871f255589710cd91ee7c64702e4221eab515de
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-10-10 20:50:39 +0100

    Fixed

--- features/auth/signup.tsx
+++ features/auth/signup.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import Image from 'next/image'
 export const SignupFeat = () => {
   return (
-    <div>
-      <h1>signup</h1>
+    <div
+    className="flex items-center "
+    >
+      <Image src="/assets/images/auth/SideImage.png" alt=""/>
+
+      {/* form */}
+      <div>
+        <h1 className="">
+        Create an account
+        </h1>
+        <p>
+        Enter your details below 
+        </p>
+     <form>
+      <input placeholder="Name" />
+      <input placeholder="Email or Phone Number" />
+      <input type="password" placeholder="Password" />
+      <button>
+      Create Account
+      </button>
+      <button>
+      Sign up with Google
+      </button>
+     </form>
+     </div>
     </div>
   )
 }
