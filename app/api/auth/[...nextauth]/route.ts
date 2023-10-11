eb40c4bb17a239a3de9eec2c7de06e3cfa26ae67
PaulAroo <timmypaul2006@gmail.com>
2023-10-11 15:49:12 +0100

    wip: setup next auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth/next"
+import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+
+      authorize(credentials, req) {
+        return null
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
