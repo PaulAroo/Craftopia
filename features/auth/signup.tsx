fdd4c9d48d8059855b70961046bad2fa3a5927c6
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-10-11 23:25:43 +0100

    signup-ui

--- features/auth/signup.tsx
+++ features/auth/signup.tsx
@@ -3,29 +3,46 @@ import Image from 'next/image'
 export const SignupFeat = () => {
   return (
     <div
-    className="flex items-center "
+    className="flex items-center"
     >
-      <Image src="/assets/images/auth/SideImage.png" alt=""/>
+      <img src="/assets/images/auth/SideImage.png" alt="" />
 
       {/* form */}
-      <div>
-        <h1 className="">
+      <div className="w-full px-32">
+        <h1 className="text-3xl font-medium">
         Create an account
         </h1>
-        <p>
+        <p className="text-base mt-6">
         Enter your details below 
         </p>
-     <form>
-      <input placeholder="Name" />
-      <input placeholder="Email or Phone Number" />
-      <input type="password" placeholder="Password" />
-      <button>
+     <div
+     className="w-full mt-12 "
+     >
+      <div>
+      <input placeholder="Name" 
+      className="border-b border-gray-900 focus:outline-none focus:border-b focus:border-gray-800 w-full "
+      />
+      </div>
+      <div>      
+        <input
+       className="border-b border-gray-900 focus:outline-none focus:border-b focus:border-gray-800 w-full mt-5"
+      placeholder="Email or Phone Number" />
+      </div>
+      <div> 
+      <input className="border-b border-gray-900 focus:outline-none focus:border-b focus:border-gray-800 w-full mt-5" type="password" placeholder="Password" />
+      </div>
+      <div>
+      <button className="w-full h-14 text-xl bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:ring focus:border-red-700 rounded mt-5">
       Create Account
       </button>
-      <button>
+      </div>
+      {/* <div>
+        <button>
       Sign up with Google
       </button>
-     </form>
+      </div> */}
+
+     </div>
      </div>
     </div>
   )
